7d3a2cd35121d4e18a447ea46c92b19dfebcbc73
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-04-30 08:45:10 -0400

    created new grid

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,80 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent (typeof(MeshFilter), typeof(MeshRenderer))]
 public class Grid : MonoBehaviour {
 
-    public int xSize, ySize;
+    public GameObject tilePrefab;
+    public int rows, cols = 10;
+    public float offset = 1;
 
-    private Vector3[] vertices;
-    private Mesh mesh;
-    private float xOffset, yOffSet;
+    private const string TILES_NAME = "Tiles";
 
 	// Use this for initialization
 	void Start () {
-	
+        CreateGrid();   
 	}
 
-    void Awake () {
-        Generate();
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void CreateGrid() {
+        float rowOffset = this.gameObject.transform.position.z;
+        float colOffset = this.gameObject.transform.position.x;
 
-    void OnDrawGizmos() {
-        if (this.vertices == null) { // prevent from throwing errors in editor mode
-            return; 
-        }
-
-        Gizmos.color = Color.black;
-        for(int i = 0; i < this.vertices.Length; i++) {
-            Gizmos.DrawSphere(this.vertices[i], 0.1f);
-        }
-    }
-
-    private void Generate() {
-        // Variable declerations 
-        int size = (this.xSize + 1) * (this.ySize + 1);
-
-      //  this.xOffset = this.xSize / 2;
-    //    this.yOffSet = this.ySize / 2;
-      //  Debug.Log("Grid Offset:" + new Vector3(xOffset, 0, yOffSet));
+        // Creates a Child element to store all the tiles
+        GameObject tiles = Instantiate(new GameObject());
+        tiles.name = TILES_NAME;
+        tiles.transform.parent = this.transform;
 
 
-        GetComponent<MeshFilter>().mesh = this.mesh = new Mesh();
-        this.mesh.name = "Procedural Grid";
+        for (int i = 0; i < this.rows; i++) {
+            rowOffset = this.gameObject.transform.position.z + (this.offset * i);
+            for(int j = 0; j < this.cols; j++) {
+                colOffset = this.gameObject.transform.position.x + (this.offset * j);
+                GameObject child = (GameObject) Instantiate(tilePrefab, new Vector3(colOffset,
+                    this.gameObject.transform.position.y, rowOffset), Quaternion.identity);
 
-        // Creates vertices
-        this.vertices = new Vector3[size];
-        Vector2[] uv = new Vector2[size];
-        Vector4[] tangenets = new Vector4[size];
-        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
-        for (int i = 0, y = 0; y <= ySize; y++) {
-            for (int x = 0; x <= xSize; x++, i++) { // increment i here since its a 1D array
-                vertices[i] = new Vector3(x, 0, y);
-                uv[i] = new Vector2((float)x / this.xSize, (float)y / ySize);
-                tangenets[i] = tangent;
+                child.transform.parent = tiles.transform;
             }
         }
-        this.mesh.vertices = vertices;
-        this.mesh.uv = uv;
-        this.mesh.tangents = tangenets;
-
-        // Creates faces
-        int[] triangles = new int[this.xSize * this.ySize * 6]; // each traingle has 3 vertices and each cube has 2 triangles 2 * 3 = 6
-        for (int ti = 0, vi = 0, y = 0; y < this.ySize; y++, vi++) {
-            for (int x = 0; x < this.xSize; x++, ti += 6, vi++) {
-                triangles[ti] = vi;
-                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
-                triangles[ti + 4] = triangles[ti + 1] = vi + this.xSize + 1;
-                triangles[ti + 5] = vi + this.xSize + 2;
-            }
-        }
-        this.mesh.triangles = triangles;
-        mesh.RecalculateNormals();
 
     }
 }
---
+++ Assets/Scripts/GridBoundary.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Grid))]
+public class GridBoundary : MonoBehaviour {
+
+    public Vector3 size = new Vector3(1f, 2f, 1f);
+    public Vector3 offset = new Vector3(0.5f, 0f, 0.5f);
+
+    private Grid grid;
+
+    private const string BOUNDARIES_NAME = "Boundaries";
+
+    // Use this for initialization
+    void Start () {
+        // Gets the Grid component
+        this.grid = this.gameObject.GetComponent<Grid>();
+
+        this.SetBoundary();
+	}
+	
+    private void SetBoundary() {
+        var rows = this.grid.rows;
+        var cols = this.grid.cols;
+
+        // Creates a parent element to store children
+        GameObject boundaries = Instantiate(new GameObject());
+        boundaries.name = BOUNDARIES_NAME;
+        boundaries.transform.parent = this.transform;
+
+        // Back Border
+        GameObject back = (GameObject) Instantiate(new GameObject(), new Vector3((cols / 2) - this.offset.x,
+            this.transform.position.y, this.transform.position.z - 1), Quaternion.identity);
+        back.name = "back";
+        back.transform.parent = boundaries.transform;
+        back.AddComponent<BoxCollider>();
+        {
+            BoxCollider colider = back.GetComponent<BoxCollider>();
+            colider.size = new Vector3(cols * this.size.x, this.size.y, this.size.z);
+        }
+
+        // Front Border
+        GameObject front = (GameObject)Instantiate(new GameObject(), new Vector3((cols / 2) - this.offset.x,
+            this.transform.position.y, this.transform.position.z + rows), Quaternion.identity);
+        front.name = "front";
+        front.transform.parent = boundaries.transform;
+        front.AddComponent<BoxCollider>();
+        {
+            BoxCollider colider = front.GetComponent<BoxCollider>();
+            colider.size = new Vector3(cols * this.size.x, this.size.y, this.size.z);
+        }
+
+        // Left Border
+        GameObject left = (GameObject)Instantiate(new GameObject(), new Vector3(this.transform.position.x - 1,
+            this.transform.position.y, (rows / 2) - this.offset.z), Quaternion.identity);
+        left.name = "left";
+        left.transform.parent = boundaries.transform;
+        left.AddComponent<BoxCollider>();
+        {
+            BoxCollider colider = left.GetComponent<BoxCollider>();
+            colider.size = new Vector3(this.size.x, this.size.y, this.size.z * rows);
+        }
+
+        // Left Border
+        GameObject right = (GameObject)Instantiate(new GameObject(), new Vector3(this.transform.position.x + cols,
+            this.transform.position.y, (rows / 2) - this.offset.z), Quaternion.identity);
+        right.name = "right";
+        right.transform.parent = boundaries.transform;
+        right.AddComponent<BoxCollider>();
+        {
+            BoxCollider colider = right.GetComponent<BoxCollider>();
+            colider.size = new Vector3(this.size.x, this.size.y, this.size.z * rows);
+        }
+    }
+}
